b9db5a45bf7fd55b5cb139c35ec80415e77d07c9
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-10-26 10:36:56 +0200

    fix setup.py / added version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = open('egov/contactdirectory/version.txt').read().strip()
+maintainer = 'Victor Baumann'
 
 setup(name='egov.contactdirectory',
       version=version,
