a95c8b3e0d407ec63d54130ba442ef92c7e8d3a7
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-10-05 10:09:12 +0200

    fix icon helper

--- egov/contactdirectory/browser/helper.py
+++ egov/contactdirectory/browser/helper.py
@@ -1,6 +1,7 @@
 def icon(item, value):
     url_method = lambda: '#'
     href = item.get('url', url_method())
+    value = value.decode('utf-8')
     if not item.get('icon'):
         return u'<a href="%s">%s</a>' % (href.decode('utf8'), value)
     return u'<a href="%s">%s%s</a>' % (href.decode('utf8'), item.get('icon'), value)
