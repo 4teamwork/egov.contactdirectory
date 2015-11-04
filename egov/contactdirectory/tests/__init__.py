346abc2b8e6c5f9526e2fc1da2ac0fb31c41fbc2
Martin Bächtold <github@tph.ch>
2015-11-04 11:29:16 +0100

    Point the link of the contact memberships to the parent object.

---
+++ egov/contactdirectory/tests/__init__.py
@@ -0,0 +1,17 @@
+from egov.contactdirectory.testing import EGOV_CONTACTDIRECTORY_FUNCTIONAL_TESTING
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from unittest2 import TestCase
+import transaction
+
+
+class FunctionalTestCase(TestCase):
+    layer = EGOV_CONTACTDIRECTORY_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def grant(self, *roles):
+        setRoles(self.portal, TEST_USER_ID, list(roles))
+        transaction.commit()
