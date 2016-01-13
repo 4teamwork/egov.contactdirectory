c2785a8f3b03495a41adb5ac4d0ba6ac2b0c1a35
Thomas Buchberger <t.buchberger@4teamwork.ch>
2016-01-13 14:10:53 +0100

    Fix sync_contacts entry point to handle parameters with spaces correctly.

--- egov/contactdirectory/sync/sync.py
+++ egov/contactdirectory/sync/sync.py
@@ -304,8 +304,10 @@ def do_sync_profiles(self, args):
     # execfile() needs the source file
     if script.endswith('.pyc') or script.endswith('.pyo'):
         script = script[:-1]
-    cmd = 'import sys; sys.argv.pop(); sys.argv.append(r\'%s\'); ' % script
-    cmd += "sys.argv[:]=%r.split(' ');" % args
+    cmd = "import sys; sys.argv[:]=[];"
+    if len(self.options.args) > 1:
+        for arg in self.options.args[1:]:
+            cmd += 'sys.argv.append(r\'%s\');' % arg
     cmd += 'execfile(r\'%s\')' % script
     cmdline = self.get_startup_cmd(self.options.python, cmd)
     exitstatus = os.system(cmdline)
